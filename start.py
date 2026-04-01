eabc4beb382d10d07d27e6cedabc8e507d359ace
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-01 16:52:41 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+router = Router()
+
+@router.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("Привіт! Я мінімальний бот на aiogram 👋")
