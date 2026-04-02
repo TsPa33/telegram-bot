38363e809ab421e884565204ae6e3320e70f48c1
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-02 23:59:41 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -5,14 +5,20 @@ from aiogram import Bot, Dispatcher
 
 from bot.config import BOT_TOKEN
 from bot.handlers import start
+from bot.database.models import create_tables
+
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def run_bot():
+    # 🔥 Ініціалізація БД (обов’язково першою)
+    create_tables()
+
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
+    # підключаємо handlers
     dp.include_router(start.router)
 
     print("BOT STARTED")
