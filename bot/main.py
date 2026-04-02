171ac122458a2fa70ec4193ec1862e2bf2d5bb88
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-02 23:19:57 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,24 @@
+import asyncio
+import logging
 
+from aiogram import Bot, Dispatcher
+
+from bot.config import BOT_TOKEN
+from bot.handlers import start
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def run_bot():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(start.router)
+
+    print("BOT STARTED")
+
+    await dp.start_polling(bot)
+
+
+def main():
+    asyncio.run(run_bot())
