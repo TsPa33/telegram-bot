9f005b99ca95f2735d7dcedc3bca275a892472ab
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-02 20:02:32 +0300

    Enhance start command with setup instructions
    Updated the start command response to include detailed setup instructions for the bot.

--- start.py
+++ start.py
@@ -14,7 +14,23 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer("Привіт! Я працюю 🚀")
+    await message.answer("Привіт! Я демо версія боту. Щоб я запрацював було потрібно 1.	Створення бота через Telegram (@BotFather)
+	2.	Отримання BOT_TOKEN
+	3.	Створення структури проєкту
+	4.	Створення проєкту локально
+	5.	Написання коду бота (start.py)
+	6.	Створення requirements.txt
+	7.	Створення Procfile
+	8.	Ініціалізація Git репозиторію
+	9.	Створення репозиторію на GitHub
+	10.	Завантаження коду (commit + push)
+	11.	Реєстрація / вхід у Railway
+	12.	Створення проєкту в Railway
+	13.	Підключення GitHub репозиторію
+	14.	Налаштування змінних середовища (BOT_TOKEN)
+	15.	Запуск деплою (Deploy)
+	16.	Перевірка логів
+	17.	Тест бота в Telegram (/start)")
 
 async def main():
     print("RUNNING MAIN...")
