93773ac607a0d703033aa65151f0774cbd5d48c3
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-02 23:36:42 +0300

    Update role.py

--- bot/keyboards/role.py
+++ bot/keyboards/role.py
@@ -1,1 +1,9 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+
+def role_keyboard():
+    kb = InlineKeyboardBuilder()
+    kb.button(text="Продавець", callback_data="role_seller")
+    kb.button(text="Покупець", callback_data="role_buyer")
+    kb.adjust(2)
+    return kb.as_markup()
