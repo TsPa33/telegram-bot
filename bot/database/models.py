489c51463a759a144b2dcb353f7a7f56cab603fb
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-02 23:55:44 +0300

    Update models.py

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,1 +1,22 @@
+from bot.database.db import cursor, conn
 
+
+def create_tables():
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        telegram_id INTEGER,
+        role TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS seller_cars (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        telegram_id INTEGER,
+        brand TEXT,
+        model TEXT
+    )
+    """)
+
+    conn.commit()
