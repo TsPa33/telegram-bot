a47e0abf58699b6824182d7f3109f5a368d2cddd
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-08 01:23:14 +0300

    Add registration states for seller in seller.py

--- bot/states/seller.py
+++ bot/states/seller.py
@@ -1,6 +1,14 @@
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.state import StatesGroup, State
 
 
 class SellerStates(StatesGroup):
+    # 🔹 додавання авто
     waiting_for_brand = State()
     waiting_for_model = State()
+
+    # 🔹 реєстрація продавця
+    reg_name = State()
+    reg_company = State()
+    reg_phone = State()
+    reg_link = State()
+    reg_city = State()
