d417b41a610229f1c445f65066d935d38c1209d9
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-08 00:36:36 +0300

    Update models.py

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,20 +1,21 @@
-from bot.database.db import get_connection
-
-def create_tables():
+def create_sellers_table():
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS seller_cars (
-            id SERIAL PRIMARY KEY,
-            telegram_id BIGINT,
-            username TEXT,
-            brand TEXT,
-            model TEXT
-        )
+    CREATE TABLE IF NOT EXISTS sellers (
+        id SERIAL PRIMARY KEY,
+        telegram_id BIGINT UNIQUE NOT NULL,
+        username TEXT,
+        name TEXT,
+        company_name TEXT,
+        phone TEXT,
+        telegram_link TEXT,
+        city TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
     """)
 
     conn.commit()
-
     cursor.close()
     conn.close()
