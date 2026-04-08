6c9004a4957865cbed8e6870062e8dd40e295598
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-08 04:44:10 +0300

    Update SellerStates with new states and comments

--- bot/states/seller.py
+++ bot/states/seller.py
@@ -2,13 +2,17 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class SellerStates(StatesGroup):
-    # 🔹 додавання авто
+    # авто
     waiting_for_brand = State()
     waiting_for_model = State()
+    waiting_for_photo = State()  # ← ОСЬ ЦЕ ГОЛОВНЕ
 
-    # 🔹 реєстрація продавця
+    # реєстрація
     reg_name = State()
     reg_company = State()
     reg_phone = State()
     reg_link = State()
     reg_city = State()
+
+    # видалення
+    delete_car = State()
