660f7963a2879fa0d362671c4d308b8a8e36082c
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-09 17:03:53 +0300

    Update db.py

--- bot/database/db.py
+++ bot/database/db.py
@@ -3,5 +3,40 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
+
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        website TEXT,
+        phone TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS brands (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER,
+        brand TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS models (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER,
+        model TEXT
+    )
+    """)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
