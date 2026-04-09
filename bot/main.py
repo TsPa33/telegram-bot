cb5937e962b80fe0c13ea0f3255e5e7111d0c12a
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-09 17:17:57 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -9,6 +9,7 @@ from bot.handlers import start, seller, buyer
 from bot.database.models import create_tables
 from bot.database.db import init_db
 from bot.handlers import start, seller, buyer
+from bot.handlers import admin
 
 logging.basicConfig(level=logging.INFO)
 
