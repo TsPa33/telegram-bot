7e13675dab8900e2c73300c85175ab8138eac08e
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-09 17:27:27 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,10 +8,8 @@ from bot.config import BOT_TOKEN
 from bot.handlers import start, seller, buyer
 from bot.database.models import create_tables
 from bot.database.db import init_db
-from bot.handlers import start, seller, buyer
 from bot.handlers import admin
 
-dp.include_router(admin.router)
 logging.basicConfig(level=logging.INFO)
 
 
@@ -19,16 +17,16 @@ async def run_bot():
     if not BOT_TOKEN:
         raise ValueError("BOT_TOKEN is not set")
 
-    await asyncio.to_thread(create_tables)
+    # await asyncio.to_thread(create_tables)
     
     init_db() 
 
     dp = Dispatcher(storage=MemoryStorage())
-    bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 
-    dp.include_router(start.router)
-    dp.include_router(buyer.router)
-    dp.include_router(seller.router)
+dp.include_router(admin.router)
+dp.include_router(buyer.router)
+dp.include_router(seller.router)
 
     logging.info("BOT STARTED")
 
