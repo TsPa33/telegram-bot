6f10367a4d82e38a6578061816f343a2fcc39ac6
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-09 17:40:06 +0300

    Update admin.py

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -1,9 +1,10 @@
 from aiogram import Router, types
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+
 from bot.config import ADMINS
 from bot.keyboards.admin_kb import admin_kb
 from bot.states.admin_states import AddUser
-from aiogram.fsm.context import FSMContext
 
 router = Router()
 
@@ -14,7 +15,9 @@ async def start_handler(message: types.Message):
         await message.answer("Admin panel", reply_markup=admin_kb)
     else:
         await message.answer("Доступ обмежений")
-        @router.message(lambda m: m.text == "➕ Додати користувача")
+
+
+@router.message(lambda m: m.text == "➕ Додати користувача")
 async def add_user_start(message: types.Message, state: FSMContext):
     await message.answer("Введіть ім'я користувача:")
     await state.set_state(AddUser.name)
