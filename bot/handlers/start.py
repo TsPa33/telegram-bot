cf8e339f03d029c293e069832667e3bd6821a659
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-10 17:54:05 +0300

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,18 +1,14 @@
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
 from bot.keyboards.role import role_keyboard
-from bot.keyboards.contact import contact_button
 from bot.keyboards.start import start_keyboard
 from bot.keyboards.seller_menu import seller_menu
 
-from bot.states.seller import SellerStates
-from bot.states.buyer import BuyerStates
-from bot.database.db import get_connection
-from bot.keyboards.brands import brand_keyboard
-from bot.keyboards.models import model_keyboard
+from bot.handlers.buyer import start_buyer
+
 router = Router()
 
 
@@ -36,6 +32,14 @@ async def start_button(message: Message):
 
 # ================= ROLE =================
 
+# 🟢 ПОКУПЕЦЬ — використовуємо існуючу логіку з buyer.py
+@router.callback_query(F.data == "role_buyer")
+async def handle_buyer(callback: CallbackQuery, state: FSMContext):
+    await start_buyer(callback.message, state)
+    await callback.answer()
+
+
+# 🟢 ПРОДАВЕЦЬ — відкриваємо меню
 @router.callback_query(F.data == "role_seller")
 async def handle_seller(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer(
@@ -43,30 +47,3 @@ async def handle_seller(callback: CallbackQuery, state: FSMContext):
         reply_markup=seller_menu()
     )
     await callback.answer()
-
-
-@router.callback_query(F.data == "role_buyer")
-async def handle_buyer(callback: CallbackQuery, state: FSMContext):
-
-    await state.set_state(BuyerStates.waiting_for_brand)
-
-    current_state = await state.get_state()
-
-    await callback.message.answer(
-        "Обери марку авто:",
-        reply_markup=brand_keyboard()
-    )
-
-    await callback.answer()
-
-# ================= VALIDATION маршутизована в іншу папку =================
-
-
-
-
-# ================= SELLER ================ #
-
-
-
-# ================= BUYER =================
-
