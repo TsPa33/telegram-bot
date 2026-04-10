f042bb537f0171afdcaee11cbf4657ff83c71c3c
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-10 21:11:36 +0300

    Update validation.py

--- bot/utils/validation.py
+++ bot/utils/validation.py
@@ -2,5 +2,5 @@ def validate_text(text: str):
     return text and text.strip()
 
 
-def normalize(text: str):
-    return text.lower().strip().capitalize()
+def normalize(text: str) -> str:
+    return text.strip().upper()
