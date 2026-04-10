f782ad32f520211bb6b0a6caea8b90c6f8cf2937
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-11 00:19:46 +0300

    Update admin_kb.py

--- bot/keyboards/admin_kb.py
+++ bot/keyboards/admin_kb.py
@@ -2,7 +2,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 admin_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="➕ Додати користувача")]
+        [KeyboardButton(text="➕ Додати користувача")],
+        [KeyboardButton(text="📋 Заявки")],
     ],
     resize_keyboard=True
 )
