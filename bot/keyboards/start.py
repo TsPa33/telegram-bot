eb71ee66595fed1b96a03fa0e3898827442e169c
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-10 18:08:38 +0300

    Update start.py

--- bot/keyboards/start.py
+++ bot/keyboards/start.py
@@ -1,9 +1,16 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from bot.config import ADMINS
 
-def start_keyboard():
+def start_keyboard(user_id=None):
+    buttons = [
+        [KeyboardButton(text="Поїхали 🚀")]
+    ]
+
+    # 🔥 Додаємо кнопку для адміна
+    if user_id in ADMINS:
+        buttons.append([KeyboardButton(text="⚙️ Адмін панель")])
+
     return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="Поїхали 🚀")]
-        ],
+        keyboard=buttons,
         resize_keyboard=True
     )
