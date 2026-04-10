6f14f8861635df5d280cdebb21a237c2423302e1
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-10 21:33:20 +0300

    Update db.py

--- bot/database/db.py
+++ bot/database/db.py
@@ -128,7 +128,7 @@ def get_models_by_brand(brand: str):
     cursor.execute("""
         SELECT DISTINCT model
         FROM models
-        WHERE UPPER(brand) = UPPER(%s)
+        WHERE LOWER(brand) = LOWER(%s)
         ORDER BY model
     """, (brand,))
 
