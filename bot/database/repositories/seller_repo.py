35d01bab3965482abfd2b81bff4e301dab304279
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-11 22:10:42 +0300

    Update seller_repo.py

--- bot/database/repositories/seller_repo.py
+++ bot/database/repositories/seller_repo.py
@@ -11,20 +11,20 @@ async def get_or_create_seller(telegram_id: int, username: str):
     """, telegram_id, username)
 
 
-async def add_seller_car(seller_id: int, model_id: int, photo_id: str):
+async def add_seller_car(seller_id: int, model_id: int, photo_id: str, description: str):
     await execute("""
-        INSERT INTO seller_cars (seller_id, model_id, photo_id)
-        VALUES ($1, $2, $3)
-    """, seller_id, model_id, photo_id)
+        INSERT INTO seller_cars (seller_id, model_id, photo_id, description)
+        VALUES ($1, $2, $3, $4)
+    """, seller_id, model_id, photo_id, description)
 
 
 async def get_seller_cars(telegram_id: int):
     rows = await fetch("""
-        SELECT m.brand, m.model
+        SELECT m.brand, m.model, sc.description
         FROM seller_cars sc
         JOIN sellers s ON sc.seller_id = s.id
         JOIN models m ON sc.model_id = m.id
         WHERE s.telegram_id = $1
     """, telegram_id)
 
-    return [(r["brand"], r["model"]) for r in rows]
+    return [(r["brand"], r["model"], r["description"]) for r in rows]
