7b4d2391b9be370be55a3fe99a2fc9936e62959f
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-11 20:34:35 +0300

    Update base.py

--- bot/database/base.py
+++ bot/database/base.py
@@ -2,15 +2,15 @@ from . import pool as pool_module
 
 
 async def fetch(query: str, *args):
-    async with pool.acquire() as conn:
+    async with pool_module.pool.acquire() as conn:
         return await conn.fetch(query, *args)
 
 
 async def fetchrow(query: str, *args):
-    async with pool.acquire() as conn:
+    async with pool_module.pool.acquire() as conn:
         return await conn.fetchrow(query, *args)
 
 
 async def execute(query: str, *args):
-    async with pool.acquire() as conn:
+    async with pool_module.pool.acquire() as conn:
         return await conn.execute(query, *args)
