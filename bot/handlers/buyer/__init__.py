a7fe5ff6355c9e24f565d4908039937a6c22f855
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-12 23:06:00 +0300

    Update __init__.py

--- bot/handlers/buyer/__init__.py
+++ bot/handlers/buyer/__init__.py
@@ -1,1 +1,12 @@
+from aiogram import Router
 
+from .start import router as start_router
+from .search import router as search_router
+from .pagination import router as pagination_router
+
+
+router = Router()
+
+router.include_router(start_router)
+router.include_router(search_router)
+router.include_router(pagination_router)
