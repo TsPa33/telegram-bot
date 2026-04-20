4af8a0226245ea451ac123c51e5d9fbf96fdfaa5
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-20 16:31:43 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,9 +4,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_IDS = [6206952389]
 
 
-def is_admin(user_id: int) -> bool:
-    return user_id in ADMIN_IDS
-
-
 # Backward compatibility for old imports.
 ADMINS = ADMIN_IDS
