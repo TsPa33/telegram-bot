3ad60fb9c626ecf5453e8bb991654ac19b769e4b
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-21 10:38:32 +0300

    Update brands.py

--- bot/keyboards/brands.py
+++ bot/keyboards/brands.py
@@ -3,10 +3,13 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def brand_kb(brands):
     rows = [
-        [InlineKeyboardButton(text=brand["name"], callback_data=f"buyer:brand:{brand['id']}")]
+        [
+            InlineKeyboardButton(
+                text=brand["name"],
+                callback_data=f"buyer:brand:{brand['id']}"
+            )
+        ]
         for brand in brands
     ]
-    rows.append([InlineKeyboardButton(text="➕ Додати бренд", callback_data="buyer:add_brand")])
-    return InlineKeyboardMarkup(
-        inline_keyboard=rows
-    )
+
+    return InlineKeyboardMarkup(inline_keyboard=rows)
