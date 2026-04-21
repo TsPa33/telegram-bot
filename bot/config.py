20c3f40b00c34f60bfeb745fcd95dd3de4c684a0
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-21 14:17:28 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,22 @@
 import os
 
+# ================= BASIC =================
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 ADMIN_IDS = [6206952389]
 
-
-# Backward compatibility for old imports.
+# Backward compatibility
 ADMINS = ADMIN_IDS
+
+
+# ================= DATABASE =================
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+# ================= LIQPAY =================
+
+LIQPAY_PUBLIC_KEY = os.getenv("LIQPAY_PUBLIC_KEY")
+LIQPAY_PRIVATE_KEY = os.getenv("LIQPAY_PRIVATE_KEY")
+LIQPAY_CALLBACK_URL = os.getenv("LIQPAY_CALLBACK_URL")
