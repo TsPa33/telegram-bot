f53bb1b58d6a2d348fd794c506c7fd5aeab9f3c3
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-21 14:24:30 +0300

    Update __init__.py

--- bot/handlers/seller/__init__.py
+++ bot/handlers/seller/__init__.py
@@ -2,14 +2,13 @@ from aiogram import Router
 
 from .add_car import router as add_car_router
 from .cars import router as cars_router
-from .verification import router as verification_router  # ✅ ДОДАНО
-
+from .verification import router as verification_router
+from .payment import router as payment_router  # ✅ ДОДАТИ
 
 router = Router()
 
-# Порядок важливий:
-# спочатку FSM (add_car), потім інші
-
+# порядок важливий
 router.include_router(add_car_router)
 router.include_router(cars_router)
-router.include_router(verification_router)  # ✅ ДОДАНО
+router.include_router(verification_router)
+router.include_router(payment_router)  # ✅ ДОДАТИ
