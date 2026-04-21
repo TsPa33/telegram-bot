aab6ac9f053ce281ca7d83e3e775444af540e8cd
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-21 14:12:51 +0300

    Create liqpay_service.py

---
+++ bot/services/liqpay_service.py
@@ -0,0 +1,33 @@
+import base64
+import json
+import hashlib
+
+
+class LiqPayService:
+
+    def __init__(self, public_key: str, private_key: str):
+        self.public_key = public_key
+        self.private_key = private_key
+        self.api_url = "https://www.liqpay.ua/api/3/checkout"
+
+    def _sign(self, data: str) -> str:
+        sign_string = self.private_key + data + self.private_key
+        return base64.b64encode(hashlib.sha1(sign_string.encode()).digest()).decode()
+
+    def generate_checkout_url(self, order_id: str, amount: int, description: str, server_url: str):
+        payload = {
+            "public_key": self.public_key,
+            "version": "3",
+            "action": "pay",
+            "amount": amount,
+            "currency": "UAH",
+            "description": description,
+            "order_id": order_id,
+            "server_url": server_url,
+        }
+
+        json_data = json.dumps(payload)
+        data = base64.b64encode(json_data.encode()).decode()
+        signature = self._sign(data)
+
+        return f"{self.api_url}?data={data}&signature={signature}"
