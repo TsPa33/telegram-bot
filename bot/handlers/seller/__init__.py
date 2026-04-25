be95618ef60bc1900639e897c14fd0ba62a81e58
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-25 13:59:23 +0300

    Update __init__.py

--- bot/handlers/seller/__init__.py
+++ bot/handlers/seller/__init__.py
@@ -1,17 +1,15 @@
 from aiogram import Router
 
-from ..seller import router as seller_main_router  # ✅ ГОЛОВНИЙ
 from .cars import router as cars_router
 from .verification import router as verification_router
 from .payment import router as payment_router
 from .profile import router as profile_router
+from .add_car import router as add_car_router
 
 router = Router()
 
-# 🔥 ГОЛОВНИЙ FLOW
-router.include_router(seller_main_router)
-
-# інші
+# Підключення всіх seller-роутерів
+router.include_router(add_car_router)
 router.include_router(cars_router)
 router.include_router(profile_router)
 router.include_router(verification_router)
