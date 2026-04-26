90c83bbe43585f1fb1f796927dc1548cf1123277
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-26 16:05:08 +0300

    Update __init__.py

--- bot/handlers/seller/__init__.py
+++ bot/handlers/seller/__init__.py
@@ -6,6 +6,7 @@ from .payment import router as payment_router
 from .profile import router as profile_router
 from .add_car import router as add_car_router
 from .services import router as services_router
+from .site import router as site_router
 
 router = Router()
 
@@ -16,3 +17,4 @@ router.include_router(profile_router)
 router.include_router(verification_router)
 router.include_router(payment_router)
 router.include_router(services_router)
+router.include_router(site_router)
