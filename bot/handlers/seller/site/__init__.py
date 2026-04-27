80eeca565744589c074480cdc40fc2310fa69688
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-27 17:15:31 +0300

    Update __init__.py

--- bot/handlers/seller/site/__init__.py
+++ bot/handlers/seller/site/__init__.py
@@ -2,8 +2,10 @@ from aiogram import Router
 
 from .menu import router as menu_router
 from .header import router as header_router
+from .banner import router as banner_router
 
 router = Router()
 
 router.include_router(menu_router)
 router.include_router(header_router)
+router.include_router(banner_router)
