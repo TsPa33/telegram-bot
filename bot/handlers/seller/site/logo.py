8c8d9a5e1d08f48f5661621b391eca232451fe8f
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-27 18:29:28 +0300

    Update logo.py

--- bot/handlers/seller/site/logo.py
+++ bot/handlers/seller/site/logo.py
@@ -1,36 +1,5 @@
-from aiogram import Router, F
-from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery, Message
-
-from bot.database.repositories.site_repo import (
-    get_site_by_seller,
-    update_site_config,
-)
-from bot.database.repositories.seller_repo import get_seller_by_telegram_id
-
-import json
-
-router = Router()
-
-
-def parse_config(config_raw):
-    if isinstance(config_raw, str):
-        return json.loads(config_raw)
-    return config_raw or {}
-
-
-@router.callback_query(F.data == "site:edit:logo")
-async def set_logo(callback: CallbackQuery, state: FSMContext):
-    await state.set_state("site_logo")
-    print("STATE SET TO site_logo")
-    await callback.message.answer("Надішліть логотип")
-    await callback.answer()
-
-
 @router.message(F.photo)
 async def save_logo(message: Message, state: FSMContext):
-    print("LOGO HANDLER TRIGGERED")
-    print("STATE:", await state.get_state())
     current_state = await state.get_state()
 
     if current_state != "site_logo":
