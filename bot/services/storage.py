a3fc87eb2f71bfeeadf5295c5b83d91cef3ae1d6
TsPa33 <Tsisar.arhitek@gmail.com>
2026-04-28 17:48:30 +0300

    Create storage.py

---
+++ bot/services/storage.py
@@ -0,0 +1,11 @@
+# bot/services/storage.py
+
+import cloudinary
+import cloudinary.uploader
+
+cloudinary.config(
+    cloud_name="YOUR_CLOUD_NAME",
+    api_key="YOUR_API_KEY",
+    api_secret="YOUR_API_SECRET",
+    secure=True
+)
