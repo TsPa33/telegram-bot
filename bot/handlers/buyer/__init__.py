bc9a887bacc49a657579520a78f5c72f1913f25f
TsPa33 <Tsisar.arhitek@gmail.com>
2026-05-07 17:33:55 +0300

    Update __init__.py

--- bot/handlers/buyer/__init__.py
+++ bot/handlers/buyer/__init__.py
@@ -2,6 +2,7 @@ from aiogram import Router
 
 from .start import router as start_router
 from .search import router as search_router
+from .services import router as services_router
 from .pagination import router as pagination_router
 
 
@@ -9,6 +10,9 @@ router = Router()
 
 # 1. Базові entry точки
 router.include_router(start_router)
+
+# 2. Послуги
+router.include_router(services_router)
 
 # 3. Потім загальні (cars/search)
 router.include_router(search_router)
